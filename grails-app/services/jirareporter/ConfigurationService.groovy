79cf65c2760a09ad7d40a0ba99edb92088090057
Administrator <farzin.zaker@aclate.com>
2019-06-26 14:21:40 -0400

    Configuration Change

--- grails-app/services/jirareporter/ConfigurationService.groovy
+++ grails-app/services/jirareporter/ConfigurationService.groovy
@@ -5,15 +5,45 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class ConfigurationService {
 
-    String getServerURL(){
+    String getServerURL() {
         ''
     }
 
-    String getUsername(){
+    String getUsername() {
         ''
     }
 
-    String getPassword(){
+    String getPassword() {
         ''
+    }
+
+    List<Map> getProjects() {
+        [
+                [name: 'Platinum-TAKE', key: 'PLTAKE'],
+                [name: 'Platinum-Beckon', key: 'PLBECK'],
+                [name: 'Platinum-NorthPlains', key: 'PLNP'],
+                [name: 'Platinum-SMS', key: 'PLSMS']
+        ]
+    }
+
+    List<String> getIssueTypes() {
+        [
+                'Bugfix', 
+                'Defect', 
+                'Development', 
+                'Documentation', 
+                'Pairing', 
+                'R&D', 
+                'Story', 
+                'Task', 
+                'Test', 
+                'Bugfix Sub-Task', 
+                'Development Sub-Task', 
+                'Documentation Sub-Task', 
+                'Pairing Sub-Task', 
+                'R&D Sub-Task', 
+                'Sub - task', 
+                'Test Sub-Task'
+        ]
     }
 }
