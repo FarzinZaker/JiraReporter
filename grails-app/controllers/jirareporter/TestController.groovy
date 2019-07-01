a21727621dacc35da617b1dfae56895e60590fa4
Administrator <farzin.zaker@aclate.com>
2019-07-01 09:41:19 -0400

    CrossOver Service Implementation

--- grails-app/controllers/jirareporter/TestController.groovy
+++ grails-app/controllers/jirareporter/TestController.groovy
@@ -1,30 +1,16 @@
 package jirareporter
 
 import grails.converters.JSON
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
 
 class TestController {
 
+    def crossOverService
 
     def index() {
 
-        def username = 'farzin.zaker@aclate.com'
-        def password = 'Retan1122'
-
-        //login
-        def get = new HttpGet('https://api.crossover.com/api/v2/timetracking/timesheets/assignment?date=2019-06-4&fullTeam=false&managerId=1721621&period=MONTH&teamId=3439')
-        get.addHeader('Accept', 'application/json, text/plain, */*')
-        get.addHeader('Authorization', "Basic ${"${username}:${password}".bytes.encodeBase64().toString()}")
-        get.addHeader('Content-Type', 'application/json;charset=UTF-8')
-        get.addHeader('Origin', 'https://app.crossover.com')
-        get.addHeader('Referer', 'https://app.crossover.com/x/dashboard/team/3439/1721621/team-timesheet?date=2019-06-03')
-        get.addHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
-
-        def client = HttpClientBuilder.create().build()
-        def response = client.execute(get)
-
-        def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
-        render bufferedReader.getText()
+        Date from = new Date() - 30
+        Date to = new Date()
+        def result = crossOverService.getWorkingHours('3439', '1721621', from, to)
+        render(result as JSON)
     }
 }
--- grails-app/services/jirareporter/CrossOverService.groovy
+++ grails-app/services/jirareporter/CrossOverService.groovy
@@ -1,29 +1,57 @@
 package jirareporter
 
+import grails.converters.JSON
 import grails.gorm.transactions.Transactional
+import groovy.time.TimeCategory
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
+import org.grails.web.json.JSONArray
+import org.grails.web.json.JSONObject
 
 @Transactional
 class CrossOverService {
 
-    def getWorkingHours() {
+    Map<String, Map<Date, Double>> getWorkingHours(String teamId, String managerId, Date from, Date to) {
 
+        def result = [:]
 
+        def maxDate = from
 
-        //login
-        def get = new HttpGet('https://api.crossover.com/api/v2/timetracking/timesheets/assignment?date=2019-06-4&fullTeam=false&managerId=1721621&period=MONTH&teamId=3439')
-        get.addHeader('Accept', 'application/json, text/plain, */*')
-        get.addHeader('Authorization', "Basic ${"${username}:${password}".bytes.encodeBase64().toString()}")
-        get.addHeader('Content-Type', 'application/json;charset=UTF-8')
-        get.addHeader('Origin', 'https://app.crossover.com')
-        get.addHeader('Referer', 'https://app.crossover.com/x/dashboard/team/3439/1721621/team-timesheet?date=2019-06-03')
-        get.addHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
+        def date = from
 
-        def client = HttpClientBuilder.create().build()
-        def response = client.execute(get)
+        while (maxDate < to) {
+            def dateStr = date.format('yyyy-MM-dd')
 
-        def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
-        render bufferedReader.getText()
+            def get = new HttpGet("https://api.crossover.com/api/v2/timetracking/timesheets/assignment?date=${dateStr}&fullTeam=true&managerId=${managerId}&period=WEEK&teamId=${teamId}")
+            get.addHeader('Accept', 'application/json, text/plain, */*')
+            get.addHeader('Authorization', "Basic ${"${Configuration.crossOverUsername}:${Configuration.crossOverPassowrd}".bytes.encodeBase64().toString()}")
+            get.addHeader('Content-Type', 'application/json;charset=UTF-8')
+            get.addHeader('Origin', 'https://app.crossover.com')
+            get.addHeader('Referer', "https://app.crossover.com/x/dashboard/team/${teamId}/${managerId}/team-timesheet?date=${dateStr}")
+            get.addHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
+
+            def client = HttpClientBuilder.create().build()
+            def response = client.execute(get)
+
+            def bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()))
+            JSONArray data = JSON.parse(bufferedReader.getText())
+            data.each { JSONObject record ->
+                if (!result.containsKey(record.name))
+                    result.put(record.name, [:])
+                record.stats.each { JSONObject stat ->
+                    def statDate = Date.parse('yyyy-MM-dd', stat.date.split('T').first())
+                    if (statDate >= from && statDate < to) {
+                        if (!result[record.name].containsKey(statDate))
+                            result[record.name].put(statDate, stat.hours)
+                    }
+                    if (statDate > maxDate)
+                        maxDate = statDate
+                }
+            }
+            use(TimeCategory) {
+                date = maxDate + 1.day
+            }
+        }
+        result
     }
 }
